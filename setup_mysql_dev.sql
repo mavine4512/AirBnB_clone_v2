61b42cf814f4629c117e15d83a61f9d9097f9617
“mavine4512” <mavineoyugi@gmail.com>
2023-11-22 13:14:43 -0800

    fixed mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,10 +3,10 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create a new user named: hbnb_dev with all privileges on the database
 -- Set the password to hbnb_dev_pwd if it does not exits
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY ' hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges to the new user on the hbnb_dev_db database
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply the changes 
 FLUSH PRIVILEGES;
