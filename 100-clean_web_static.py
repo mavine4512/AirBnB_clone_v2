c419047f4f90e377f081296a1f68c25b6a47a5a3
Deey-Kerry <dianahkerrynyash@gmail.com>
2023-11-22 15:34:01 -0800

    additions

---
+++ 100-clean_web_static.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+import os
+from fabric.api import *
+
+env.hosts = ['52.86.139.197', '100.24.74.142']
+
+
+def do_clean(number=0):
+    """Cleans the files"""
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
