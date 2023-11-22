5aae5fed151f8ca501cb8e4b4bd522f6183d0d67
“mavine4512” <mavineoyugi@gmail.com>
2023-11-22 15:31:12 -0800

    fixed web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
+
+if __name__ == "__main__"
+app.run(host="0.0.0.0", post=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def route_hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
+@app.route('/hbnb/', strict_slashes=False)
+def route_hbnb():
+    """Return a given string"""
+    return ("HBNB")
+
+@app.route('/c/<text>', strict_slashes=False)
+def route_cText(text):
+    """Display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+if __name__ == "__main__"
+app.run(host="0.0.0.0", post=5000, debug=None)
