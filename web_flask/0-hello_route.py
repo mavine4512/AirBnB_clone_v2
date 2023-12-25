a06f106f4d27ba046af55d9cec40ee6c91942cb4
“mavine4512” <mavineoyugi@gmail.com>
2023-12-25 07:39:23 -0800

    fixed 0-hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,11 +7,11 @@ from flask import Flask
 app = Flask("__name__")
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """Return a given string"""
     return ("Hello HBNB!")
 
 
-if __name__ == "__main__"
-app.run(host="0.0.0.0", post=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", post=5000, debug=None)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,10 @@ def hello():
     """Return a given string"""
     return ("Hello HBNB!")
 
-@app.route('/hbnb/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return a given string"""
     return ("HBNB")
 
-if __name__ == "__main__"
-app.run(host="0.0.0.0", post=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", post=5000, debug=None)
