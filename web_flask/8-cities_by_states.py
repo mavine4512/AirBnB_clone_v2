412e73eee4312c9c12b101dc320636724e186d15
“mavine4512” <mavineoyugi@gmail.com>
2023-12-27 05:41:16 -0800

    fixed items

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -18,10 +18,10 @@ def close(self):
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """Displays a html page with states and cities"""
-    state = storage.all(State)
+    states = storage.all(State)
     cities = storage.all(City)
     return render_template(
-            '8-cities_by_states.html',states=states, cities=cities)
+            '8-cities_by_states.html', states=states, cities=cities)
 
 
 if __name__ == "__main__":
